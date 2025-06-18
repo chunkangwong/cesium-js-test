b6b6c46b251416690334ae9eeb3e706706ee2948
chunkangwong <chunkang.wong94@gmail.com>
2025-06-18 22:16:11 +0800

    Add base cesium

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,35 @@
-import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
+declare global {
+  interface Window {
+    CESIUM_BASE_URL: string;
+  }
+}
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+window.CESIUM_BASE_URL = "/cesium";
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+import {
+  Cartesian3,
+  Ion,
+  Math,
+  Terrain,
+  Viewer,
+  createOsmBuildingsAsync,
+} from "cesium";
+
+Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ACCESS_TOKEN;
+
+const viewer = new Viewer("container", {
+  terrain: Terrain.fromWorldTerrain(),
+});
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+  destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+  orientation: {
+    heading: Math.toRadians(0.0),
+    pitch: Math.toRadians(-15.0),
+  },
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+createOsmBuildingsAsync().then((buildingTileset) => {
+  viewer!.scene.primitives.add(buildingTileset);
+});
